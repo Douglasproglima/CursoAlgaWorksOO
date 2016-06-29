1998d1d9a43c3e009cc9a23b591d754ed8cf27ba
Douglas Lima <douglasproglima@gmail.com>
2016-06-29 10:55:28 -0300

    Coletor de Lixo (garbage collection)

---
+++ 6 - TópicoAvançado/ColetaDeLixo/ProgramaColetaLixo.java
@@ -0,0 +1,26 @@
+package ColetaDeLixo;
+
+import Interfaces.NotaFiscal;
+import SobrecargaMetodos.Pessoa;
+
+public class ProgramaColetaLixo {
+	public static void main(String[] args){
+		/*Quando se cria um objeto o mesmo fica armazenado na memória heap
+		 * Carro carro    = -----> objeto1
+		 * NotaFiscal nfe = -----> objeto2
+		 * Pessoa pessoa  = -----> objeto3
+		 * */
+		
+		NotaFiscal nfe = new NotaFiscal(132456);
+		nfe.imprimirAlgo();
+		
+		//Elimina a referência do objeto na mémoria. Obs: O objeto2 ainda está na heap
+		nfe = null;
+		
+		//Erro de NullPointerException -> O objeto não tem referência para o objeto
+		nfe.imprimirAlgo();
+		
+		
+		 
+	}
+}
